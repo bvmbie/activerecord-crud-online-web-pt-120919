1545dfa5a74fe113950ea9b0b1cb5fd52c5cc53f
Henna Vahatiitto <hnnamv@gmail.com>
2020-03-04 18:53:06 +0000

    Automatically backed up by Learn

--- db/migrate/20200304184242_create_movies.rb
+++ db/migrate/20200304184242_create_movies.rb
@@ -1,9 +1,10 @@
 class CreateMovies < ActiveRecord::Migration[5.2]
   def change
-    add_column :movie, :title, :string
-    add_column :movie, :release_date, :integer
-    add_column :movie, :director, :string
-    add_column :movie, :lead, :string 
-    add_column :movie, :in_theaters, :boolean
+    create_table :movies do |t|
+      t.string :title
+      t.integer :release_date
+      t.string :director
+      t.string :lead
+      t.boolean :in_theaters
   end
 end
