19ddd8c4b9e465a783f09bbfa0ff640e81561766
Henna Vahatiitto <hnnamv@gmail.com>
2020-03-04 18:47:04 +0000

    Automatically backed up by Learn

--- db/migrate/20200304184242_create_movies.rb
+++ db/migrate/20200304184242_create_movies.rb
@@ -1,5 +1,9 @@
 class CreateMovies < ActiveRecord::Migration[5.2]
   def change
     add_column :movies, :title, :string
+    add_column :movies, :release_date
+    add_column :movies, 
+    add_column :movies, 
+    add_column :movies, 
   end
 end
